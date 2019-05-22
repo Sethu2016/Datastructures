a31de938a8abcce8e99f22544a399867a398a8d4
Sethu <sethu.raghavendran@gmail.com>
2019-05-22 06:36:22 -0400

    Latest minor changes

---
+++ src/com/heap/Heap.java
@@ -0,0 +1,17 @@
+package com.heap;
+
+/*Algorithm
+Heapify (Min Heap)
+2) Take first element of sorted array
+3) Replace last element at first position
+4) Check if tree is heap
+if yes goto step 2
+If No goto step 1
+
+root = i
+left child index = 2i+1
+right child index = 2i+2
+*/
+
+public class Heap {
+}
